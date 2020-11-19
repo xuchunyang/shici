ea2430e16015e72d76c9b2563bc03e7139a5f016
Xu Chunyang <xuchunyang56@gmail.com>
2020-11-19 20:57:51 +0800

    * api/topAuthors.js: 输出作品数量.

--- api/topAuthors.js
+++ api/topAuthors.js
@@ -4,7 +4,18 @@ module.exports = async (req, res) => {
   let { limit } = req.query;
   limit = parseInt(limit) || 10;
   const [shi, ci] = await topAuthors(limit);
-  const shici = shi.map(({ _id }) => _id).concat(ci.map(({ _id }) => _id));
-  const authors = Array.from(new Set(shici));
-  res.json({ count: authors.length, data: authors });
+  let authors = {};
+  shi.forEach(({ _id: author, number }) => {
+    authors[author] = number;
+  });
+  ci.forEach(({ _id: author, number }) => {
+    if (author in authors) authors[author] += number;
+    else authors[author] = number;
+  });
+  const data = Object.entries(authors)
+    .map(([author, number]) => {
+      return { author, number };
+    })
+    .sort(({ number: n1 }, { number: n2 }) => n2 - n1);
+  res.json({ count: data.length, data });
 };
